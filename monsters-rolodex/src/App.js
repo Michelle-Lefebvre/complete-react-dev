5fa2e5afb479a504cdae210603e82d7fdafa8702
Michelle Lefebvre <mmldesigns007@gmail.com>
2022-04-16 22:15:34 -0400

    fetch API, change favicon

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,33 +1,38 @@
 import "./App.css";
 import { Component } from "react";
-import Certificates from "./components/certificates/Certificates";
+// import Certificates from "./components/certificates/Certificates";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: "Linda",
-        },
-        {
-          name: "Frank",
-        },
-        {
-          name: "Jackie",
-        },
-      ],
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
 
   render() {
     return (
       <div className="App">
         {this.state.monsters.map((monster) => {
-          return <h1 key={monster.name}>{monster.name}</h1>;
+          return <h1 key={monster.id}>{monster.name}</h1>;
         })}
 
-        <Certificates />
+        {/* <Certificates /> */}
       </div>
     );
   }
