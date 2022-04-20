5cd115a00ecc7a2cf72349c4c6edb74401e57134
Michelle Lefebvre <mmldesigns007@gmail.com>
2022-04-20 15:40:38 -0400

    add firebase pkg, add signin comp.

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/routes/home/home.component";
 import Navigation from "./components/routes/navigation/navigation.component";
+import SignIn from "./components/routes/signin/signin.component";
 
 const Shop = () => {
   return <h1>I am the Shop page</h1>;
@@ -12,6 +13,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
